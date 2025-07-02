a488ac5a39e6fcd5a2121b4d3d064f00464e951e
Tomas Gutierrez <gutierreztomas321@gmail.com>
2025-07-02 02:29:02 -0400

    retry memory saver

--- backend/main.py
+++ backend/main.py
@@ -15,7 +15,7 @@ from langchain.prompts import PromptTemplate
 from newsapi import NewsApiClient
 from typing import Optional
 from langchain.tools import tool
-from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.checkpoint.memory import MemorySaver
 
 from dotenv import load_dotenv
 
@@ -208,7 +208,7 @@ try:
 
     tools = [tavily_search, news_top_headlines, news_everything, news_sources, news_event_summary]
 
-    checkpointer = InMemorySaver()
+    checkpointer = MemorySaver()
 
     # Create the agent with the prompt template
     agent = create_react_agent(model, tools, REACT_PROMPT, checkpointer=checkpointer)
